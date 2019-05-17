dc91931d3c8cfd795d295e6850359ca8190f1004
TinaDelli <cecurrier92@gmail.com>
2019-05-17 12:24:51 -0400

    answered questions, added in the ability to pull the data and add in smurfs to the app

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,35 @@
+import axios from 'axios';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const FETCH_SMURFS_START = "FETCH_SMURFS_START";
+export const FETCH_SMURFS_SUCCESS = "FETCH_SMURFS_SUCCESS";
+export const FETCH_SMURFS_FAILURE = "FETCH_SMURFS_FAILURE";
 
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCH_SMURFS_START });
+  axios
+  .get('http://localhost:3333/smurfs')
+  .then(res => {
+    dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data });
+  })
+  .catch(err => console.log(err))
+}
+
+export const ADD_SMURFS_START = "ADD_SMURFS_START";
+export const ADD_SMURFS_SUCCESS = "ADD_SMURFS_SUCCESS";
+export const ADD_SMURFS_FAILURE = "ADD_SMURFS_FAILURE";
+
+export const addSmurf = smurf => dispatch => {
+  dispatch({ type: ADD_SMURFS_START});
+  return axios
+  .post('http://localhost:3333/smurfs', smurf)
+  .then(res => {
+    dispatch({ type: ADD_SMURFS_SUCCESS, payload: res.data });
+  })
+  .catch(err => console.log(err))
+}
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,14 @@
+import { FETCH_SMURFS_START, FETCH_SMURFS_SUCCESS, FETCH_SMURFS_FAILURE, ADD_SMURFS_START, ADD_SMURFS_SUCCESS, ADD_SMURFS_FAILURE } from '../actions';
 /*
   Be sure to import in all of the action types from `../actions`
 */
+
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  error: null
+}
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -14,6 +22,38 @@
  }
 */
 
+const reducer = (state = initialState, action) => {
+  switch(action.type){
+    case FETCH_SMURFS_START: 
+    return {
+      ...state,
+      fetchingSmurfs: true
+    };
+    case FETCH_SMURFS_SUCCESS:
+    return {
+      ...state,
+      error: '',
+      fetchingSmurfs: false,
+      smurfs: action.payload
+    };
+    case ADD_SMURFS_START:
+    return {
+      ...state,
+      addingSmurf: true
+    };
+    case ADD_SMURFS_SUCCESS:
+    return {
+      ...state,
+      addingSmurf: false,
+      error:'',
+      smurfs: action.payload
+    };
+    default:
+    return state;
+  }
+}
+
+export default reducer;
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
